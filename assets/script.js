6d45c9cf989c07007b70314b17942a63094c2258
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2022-09-29 12:51:34 +0700

    Memperbaiki landing page responsive

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
 const navbar = document.querySelector(".navbar");
+// Create a media condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia("(max-width: 400px)");
 window.onscroll = () => {
   if (window.scrollY > 250) {
     navbar.classList.add("bg-dark");
   } else {
+    if (mediaQuery.matches) {
+      // Then trigger an alert
+      navbar.classList.add("bg-dark");
+    }
     navbar.classList.remove("bg-dark");
   }
 };
 
-// Create a media condition that targets viewports at least 768px wide
-const mediaQuery = window.matchMedia("(max-width: 400px)");
 // Check if the media query is true
 if (mediaQuery.matches) {
   // Then trigger an alert
